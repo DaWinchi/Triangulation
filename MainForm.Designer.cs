5ab306d778556ccea04d7e30f48653ff9f60dea7
prutskov <lehaprutskov@gmail.com>
2018-03-06 01:09:29 +0300

    Visual changes and update interface with user

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,12 +32,22 @@
             this.GraphicsBox = new System.Windows.Forms.PictureBox();
             this.DrawBtn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ScrollHeight = new System.Windows.Forms.TrackBar();
+            this.ScrollWidth = new System.Windows.Forms.TrackBar();
+            this.RadioMagnet1 = new System.Windows.Forms.RadioButton();
+            this.RadioMagnet2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ScrollAngle = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.GraphicsBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollWidth)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollAngle)).BeginInit();
             this.SuspendLayout();
             // 
             // GraphicsBox
             // 
-            this.GraphicsBox.Location = new System.Drawing.Point(24, 14);
+            this.GraphicsBox.Location = new System.Drawing.Point(53, 12);
             this.GraphicsBox.Name = "GraphicsBox";
             this.GraphicsBox.Size = new System.Drawing.Size(600, 600);
             this.GraphicsBox.TabIndex = 0;
@@ -45,11 +55,11 @@
             // 
             // DrawBtn
             // 
-            this.DrawBtn.Location = new System.Drawing.Point(809, 419);
+            this.DrawBtn.Location = new System.Drawing.Point(809, 558);
             this.DrawBtn.Name = "DrawBtn";
             this.DrawBtn.Size = new System.Drawing.Size(136, 63);
             this.DrawBtn.TabIndex = 1;
-            this.DrawBtn.Text = "button1";
+            this.DrawBtn.Text = "Начать";
             this.DrawBtn.UseVisualStyleBackColor = true;
             this.DrawBtn.Click += new System.EventHandler(this.DrawBtn_Click);
             // 
@@ -58,17 +68,88 @@
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // ScrollHeight
+            // 
+            this.ScrollHeight.Location = new System.Drawing.Point(2, 12);
+            this.ScrollHeight.Name = "ScrollHeight";
+            this.ScrollHeight.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.ScrollHeight.Size = new System.Drawing.Size(45, 600);
+            this.ScrollHeight.TabIndex = 2;
+            this.ScrollHeight.Scroll += new System.EventHandler(this.ScrollHeight_Scroll);
+            // 
+            // ScrollWidth
+            // 
+            this.ScrollWidth.Location = new System.Drawing.Point(53, 618);
+            this.ScrollWidth.Name = "ScrollWidth";
+            this.ScrollWidth.Size = new System.Drawing.Size(600, 45);
+            this.ScrollWidth.TabIndex = 3;
+            this.ScrollWidth.Scroll += new System.EventHandler(this.ScrollWidth_Scroll);
+            // 
+            // RadioMagnet1
+            // 
+            this.RadioMagnet1.AutoSize = true;
+            this.RadioMagnet1.Checked = true;
+            this.RadioMagnet1.Location = new System.Drawing.Point(6, 19);
+            this.RadioMagnet1.Name = "RadioMagnet1";
+            this.RadioMagnet1.Size = new System.Drawing.Size(71, 17);
+            this.RadioMagnet1.TabIndex = 4;
+            this.RadioMagnet1.TabStop = true;
+            this.RadioMagnet1.Text = "Магнит 1";
+            this.RadioMagnet1.UseVisualStyleBackColor = true;
+            this.RadioMagnet1.CheckedChanged += new System.EventHandler(this.RadioMagnet1_CheckedChanged);
+            // 
+            // RadioMagnet2
+            // 
+            this.RadioMagnet2.AutoSize = true;
+            this.RadioMagnet2.Location = new System.Drawing.Point(6, 42);
+            this.RadioMagnet2.Name = "RadioMagnet2";
+            this.RadioMagnet2.Size = new System.Drawing.Size(71, 17);
+            this.RadioMagnet2.TabIndex = 5;
+            this.RadioMagnet2.Text = "Магнит 2";
+            this.RadioMagnet2.UseVisualStyleBackColor = true;
+            this.RadioMagnet2.CheckedChanged += new System.EventHandler(this.RadioMagnet2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RadioMagnet2);
+            this.groupBox1.Controls.Add(this.RadioMagnet1);
+            this.groupBox1.Location = new System.Drawing.Point(710, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(110, 68);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите магнит";
+            // 
+            // ScrollAngle
+            // 
+            this.ScrollAngle.Location = new System.Drawing.Point(659, 12);
+            this.ScrollAngle.Name = "ScrollAngle";
+            this.ScrollAngle.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.ScrollAngle.Size = new System.Drawing.Size(45, 600);
+            this.ScrollAngle.TabIndex = 7;
+            this.ScrollAngle.Scroll += new System.EventHandler(this.ScrollAngle_Scroll);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(957, 633);
+            this.ClientSize = new System.Drawing.Size(957, 664);
+            this.Controls.Add(this.ScrollAngle);
+            this.Controls.Add(this.ScrollWidth);
+            this.Controls.Add(this.ScrollHeight);
             this.Controls.Add(this.DrawBtn);
             this.Controls.Add(this.GraphicsBox);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Триангуляция Делоне";
             ((System.ComponentModel.ISupportInitialize)(this.GraphicsBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollWidth)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ScrollAngle)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +158,12 @@
         private System.Windows.Forms.PictureBox GraphicsBox;
         private System.Windows.Forms.Button DrawBtn;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar ScrollHeight;
+        private System.Windows.Forms.TrackBar ScrollWidth;
+        private System.Windows.Forms.RadioButton RadioMagnet1;
+        private System.Windows.Forms.RadioButton RadioMagnet2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar ScrollAngle;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -24,8 +24,24 @@ namespace TriangleDeloneWithMagnetic
                 xmin = -100,
                 ymin = -100
             };
+            points = new List<PointF>();
+            magnet1 = new Magnet(40, 20, center1, (float)(2 * Math.PI / 360 * 45), 5);
+            magnet2 = new Magnet(40, 20, center2, (float)(2 * Math.PI / 360 * 30), 5);
+            GlobalRect = new Magnet(160, 160, centerGlobal, (float)(2 * Math.PI / 360 * 0), 20);
+            points.AddRange(magnet1.ReturnRectangleDdiscret()); points.AddRange(magnet2.ReturnRectangleDdiscret());
+            points.AddRange(GlobalRect.ReturnRectangleDdiscret());
+            ScrollHeight.Minimum = (int)GlobalRect.A.Y;
+            ScrollHeight.Value = (int)magnet1.center.Y;
+            ScrollHeight.Maximum = (int)GlobalRect.D.Y;
 
+            ScrollWidth.Minimum = (int)GlobalRect.A.X;
+            ScrollWidth.Value = (int)magnet1.center.X;
+            ScrollWidth.Maximum = (int)GlobalRect.B.X;
 
+            ScrollAngle.Minimum = 0;
+            ScrollAngle.Maximum = 360;
+            ScrollAngle.Value=45;
+            Painting();
         }
 
         Painter parametrs;
@@ -33,12 +49,82 @@ namespace TriangleDeloneWithMagnetic
         Graphics graph;
         List<PointF> points;
         Triangulation triangulation;
+        Magnet magnet1; PointF center1 = new PointF(0, -50);
+        Magnet magnet2; PointF center2 = new PointF(0, 50);
+        Magnet GlobalRect; PointF centerGlobal = new PointF(0, 0);
 
-        Magnet GlobalRect;
-        
         float step_x = 7F, step_y = 7F, x_now, y_now;
 
         List<Triangle> list_triangles = new List<Triangle>();
+
+        private void ScrollHeight_Scroll(object sender, EventArgs e)
+        {
+            int y = ScrollHeight.Value;
+            if (RadioMagnet1.Checked)
+            {
+                magnet1.center.Y = y;
+            }
+
+            if (RadioMagnet2.Checked)
+            {
+                magnet2.center.Y = y;
+            }
+            points.Clear();
+            points.AddRange(magnet1.ReturnRectangleDdiscret()); points.AddRange(magnet2.ReturnRectangleDdiscret());
+            points.AddRange(GlobalRect.ReturnRectangleDdiscret());
+            Painting();
+
+        }
+
+        private void ScrollWidth_Scroll(object sender, EventArgs e)
+        {
+            int x = ScrollWidth.Value;
+            if (RadioMagnet1.Checked)
+            {
+                magnet1.center.X = x;
+            }
+
+            if (RadioMagnet2.Checked)
+            {
+                magnet2.center.X = x;
+            }
+            points.Clear();
+            points.AddRange(magnet1.ReturnRectangleDdiscret()); points.AddRange(magnet2.ReturnRectangleDdiscret());
+            points.AddRange(GlobalRect.ReturnRectangleDdiscret());
+            Painting();
+        }
+
+        private void ScrollAngle_Scroll(object sender, EventArgs e)
+        {
+            float angle = (float)(2 * Math.PI / 360 *ScrollAngle.Value);
+            if (RadioMagnet1.Checked)
+            {
+                magnet1.angle = angle;
+            }
+
+            if (RadioMagnet2.Checked)
+            {
+                magnet2.angle=angle;
+            }
+            points.Clear();
+            points.AddRange(magnet1.ReturnRectangleDdiscret()); points.AddRange(magnet2.ReturnRectangleDdiscret());
+            points.AddRange(GlobalRect.ReturnRectangleDdiscret());
+            Painting();
+        }
+
+        private void RadioMagnet1_CheckedChanged(object sender, EventArgs e)
+        {
+            ScrollHeight.Value = (int)magnet1.center.Y;
+            ScrollWidth.Value = (int)magnet1.center.X;
+            ScrollAngle.Value = (int)(magnet1.angle * 360 / 2 / Math.PI);
+        }
+
+        private void RadioMagnet2_CheckedChanged(object sender, EventArgs e)
+        {
+            ScrollHeight.Value = (int)magnet2.center.Y;
+            ScrollWidth.Value = (int)magnet2.center.X;
+            ScrollAngle.Value = (int)(magnet2.angle * 360 / 2 / Math.PI);
+        }
 
         private void Painting()
         {
@@ -68,6 +154,12 @@ namespace TriangleDeloneWithMagnetic
 
             }
 
+            foreach (PointF point in points)
+            {
+                graph.FillRectangle(brushPoint, (float)parametrs.X(width, point.X) - 2, (float)parametrs.Y(height, point.Y) - 2, 4, 4);
+
+            }
+
             GraphicsBox.Image = bmp;
         }
 
@@ -75,15 +167,7 @@ namespace TriangleDeloneWithMagnetic
         {
             triangulation = new Triangulation();
             list_triangles.Clear();
-            PointF center1 = new PointF(0, -50);
-            float angle1 = (float)(2 * Math.PI / 360 * 45);
-            PointF center2 = new PointF(0, 50);
-            float angle2 = (float)(2 * Math.PI / 360 * 30);
-            PointF centerGlobal = new PointF(0, 0);
 
-            Magnet magnet1 = new Magnet(40, 20, center1, angle1, 5);
-            Magnet magnet2 = new Magnet(40, 20, center2, angle2, 5);
-            GlobalRect = new Magnet(160, 160, centerGlobal, (float)(2 * Math.PI / 360 * 0), 20);           
             PointF point1 = new PointF(300, 0); PointF point2 = new PointF(-300, 0);
             PointF point3 = new PointF(0, 300); PointF point4 = new PointF(0, -300);
 
@@ -97,7 +181,7 @@ namespace TriangleDeloneWithMagnetic
             x_now = GlobalRect.A.X + step_x;
             y_now = GlobalRect.A.Y + step_y;
 
-           timer1.Start();
+            timer1.Start();
             Painting();
         }
 
@@ -106,13 +190,15 @@ namespace TriangleDeloneWithMagnetic
             if (x_now > GlobalRect.C.X)
             {
                 y_now += step_y;
-                if (y_now > GlobalRect.C.Y-step_y/2) {
-                    timer1.Stop(); list_triangles.Clear(); list_triangles.AddRange(triangulation.SortTriangle()); Painting(); }
+                if (y_now > GlobalRect.C.Y - step_y / 2)
+                {
+                    timer1.Stop(); list_triangles.Clear(); list_triangles.AddRange(triangulation.SortTriangle()); Painting();
+                }
                 x_now = GlobalRect.A.X;
             }
 
             Random rand = new Random();
-            PointF point = new PointF(x_now+(float)rand.NextDouble()/100, y_now+(float)rand.NextDouble() / 100);
+            PointF point = new PointF(x_now + (float)rand.NextDouble() / 100, y_now + (float)rand.NextDouble() / 100);
             list_triangles.Clear();
             list_triangles.AddRange(triangulation.AddPoint(point));
             x_now += step_x;
@@ -121,5 +207,5 @@ namespace TriangleDeloneWithMagnetic
         }
     }
 
-    
+
 }
