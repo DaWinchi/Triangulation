69568cd95c3e81cd0d3a7afda4356ba0c2e81444
prutskov <lehaprutskov@gmail.com>
2018-03-13 21:36:00 +0300

    Implemented class Galerkin

---
+++ Galerkin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleDeloneWithMagnetic
+{
+    public struct Piramide { List<Triangle> base_triangles; PointF vertex; float value; }
+    public class Galerkin
+    {
+        public List<Piramide> piramides;
+        private List<Triangle> triangles;
+        private List<PointF> points;
+        public Galerkin  (List<Triangle> p_triangles, List<PointF> p_points)
+        {
+            triangles = new List<Triangle>();
+            triangles.AddRange(p_triangles);
+
+            points = new List<PointF>();
+            points.AddRange(p_points);
+
+            piramides = new List<Piramide>();
+            
+        }
+
+
+        private 
+            
+    }
+}
